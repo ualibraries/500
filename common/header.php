758d39aaaaebedaf247f7857bdb836a9ca39c91e
Ginger Bidwell <liquid06@email.arizona.edu>
2017-07-18 17:27:32 -0700

    Link theme logo to exhibit homepage. Refs #6
    A plugin provides our omeka-wide navigation, and the homepage link is part of that. Since this theme will be on of an exhibit, it will need to link to the exhibit's homepage.

--- common/header.php
+++ common/header.php
@@ -43,7 +43,9 @@
                 <?php echo exhibit_builder_page_tree($exhibit, $exhibit_page); ?>
             </nav>
 
-            <div id="site-title"><?php echo link_to_home_page(theme_logo()); ?></div>
+            <div id="site-title">
+                <?php echo exhibit_builder_link_to_exhibit($exhibit, theme_logo()); ?>
+            </div>
 
             <a href="<?php echo url(get_theme_option('header_secondary_link')); ?>"><?php echo get_theme_option('header_secondary_link_text'); ?></a>
 
