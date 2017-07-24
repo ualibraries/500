f8abae296201d481ef6688042a7afff9c310d67c
Christopher Green <cg433n@email.arizona.edu>
2017-07-24 14:38:40 -0700

    Themes exhibit pagination Refs #8

--- exhibit-builder/exhibits/show.php
+++ exhibit-builder/exhibits/show.php
@@ -2,30 +2,34 @@
 echo head(array(
     'title' => metadata('exhibit_page', 'title') . ' &middot; ' . metadata('exhibit', 'title'),
     'bodyclass' => 'exhibits show'));
-?>
-<div class="exhibit-page-contents">
-<h1><span class="exhibit-page"><?php echo metadata('exhibit_page', 'title'); ?></span></h1>
+    ?>
+    <div class="exhibit-page-contents">
+        <h1><span class="exhibit-page"><?php echo metadata('exhibit_page', 'title'); ?></span></h1>
 
-<div class="exhibit-blocks">
-<?php exhibit_builder_render_exhibit_page(); ?>
-</div>
+        <div class="exhibit-blocks">
+            <?php exhibit_builder_render_exhibit_page(); ?>
+        </div>
 
-<div id="exhibit-page-navigation">
-    <?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
-    <div id="exhibit-nav-prev">
-    <?php echo $prevLink; ?>
+        <div class="exhibit-page-navigation">
+            <?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
+                <div class="exhibit-nav-prev">
+                    <div class="exhibit-nav-prev-inner">
+                        <?php echo $prevLink; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+            <?php if ($nextLink = exhibit_builder_link_to_next_page()): ?>
+                <div class="exhibit-nav-next">
+                    <div class="exhibit-nav-next-inner">
+                        <?php echo $nextLink; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+            <div class="exhibit-nav-up">
+                <?php echo exhibit_builder_page_trail(); ?>
+            </div>
+        </div>
+
     </div>
-    <?php endif; ?>
-    <?php if ($nextLink = exhibit_builder_link_to_next_page()): ?>
-    <div id="exhibit-nav-next">
-    <?php echo $nextLink; ?>
-    </div>
-    <?php endif; ?>
-    <div id="exhibit-nav-up">
-    <?php echo exhibit_builder_page_trail(); ?>
-    </div>
-</div>
 
-</div>
-
-<?php echo foot(); ?>
+    <?php echo foot(); ?>
