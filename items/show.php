7659c1cc6b40369246ad08b1b437e5baf7af6d43
Andy Osborne <cao89@email.arizona.edu>
2017-07-25 08:03:12 -0700

    Display file level metadata to beneath each file refs #19

--- items/show.php
+++ items/show.php
@@ -44,6 +44,42 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
                         <img src="<?php print $file_fullsize_image; ?>" alt="<?php print $file_alt; ?>" />
                     </a>
                     <p class="file-caption"><?php echo $file_title; ?></p>
+                    <div class="all-metadata">
+                        <div id="text-metadata">
+                            <h2><?php echo __('File Metadata'); ?></h2>
+                            <?php echo all_element_texts($file, ['show_element_set_headings' => false]); ?>
+                        </div>
+                        <div id="format-metadata">
+                            <h2><?php echo __('Format Metadata'); ?></h2>
+                            <div id="original-filename" class="element">
+                                <h3><?php echo __('Original Filename'); ?></h3>
+                                <div class="element-text"><?php echo metadata('file', 'Original Filename'); ?></div>
+                            </div>
+
+                            <div id="file-size" class="element">
+                                <h3><?php echo __('File Size'); ?></h3>
+                                <div class="element-text"><?php echo __('%s bytes', metadata('file', 'Size')); ?></div>
+                            </div>
+
+                            <div id="authentication" class="element">
+                                <h3><?php echo __('Authentication'); ?></h3>
+                                <div class="element-text"><?php echo metadata('file', 'Authentication'); ?></div>
+                            </div>
+                        </div><!-- end format-metadata -->
+
+                        <div id="type-metadata" class="section">
+                            <h2><?php echo __('Type Metadata'); ?></h2>
+                            <div id="mime-type-browser" class="element">
+                                <h3><?php echo __('Mime Type'); ?></h3>
+                                <div class="element-text"><?php echo metadata('file', 'MIME Type'); ?></div>
+                            </div>
+                            <div id="file-type-os" class="element">
+                                <h3><?php echo __('File Type / OS'); ?></h3>
+                                <div class="element-text"><?php echo metadata('file', 'Type OS'); ?></div>
+                            </div>
+                        </div><!-- end type-metadata -->
+
+                    </div>
                 </div>
 
             <?php endforeach; ?>
