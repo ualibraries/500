12740e40572c93239ac6154b38a927308ab5487f
Christopher Green <cg433n@email.arizona.edu>
2017-07-25 10:04:32 -0700

    Adds exhibit homepage image theme option and styles Refs #9

--- exhibit-builder/exhibits/show.php
+++ exhibit-builder/exhibits/show.php
@@ -4,6 +4,24 @@ echo head(array(
     'bodyclass' => 'exhibits show'));
     ?>
     <div class="exhibit-page-contents">
+
+        <?php
+        // Display the exhibit homepage image (set as a theme option)
+        $current_slug = get_current_record('exhibit_page', false)->slug;
+        if ($current_slug === get_theme_option('homepage_slug')):
+        ?>
+            <div class="exhibit-homepage-image">
+                <?php
+                $homepage_image = get_theme_option('homepage_image');
+                if ($homepage_image) {
+                    $storage = Zend_Registry::get('storage');
+                    $uri = $storage->getUri($storage->getPathByType($homepage_image, 'theme_uploads'));
+                    echo '<img src="' . $uri . '" alt="' . option('site_title') . '" />';
+                }
+                ?>
+            </div>
+        <?php endif; ?>
+
         <h1 class="exhibit-page-title"><?php echo metadata('exhibit_page', 'title'); ?></h1>
 
         <div class="exhibit-blocks">
