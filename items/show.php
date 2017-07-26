707f1d200ae246cb1e93c746b7277bcfd2eb39d6
Christopher Green <cg433n@email.arizona.edu>
2017-07-26 14:30:09 -0700

    Adds file meta info modal coolness Refs #17

--- items/show.php
+++ items/show.php
@@ -49,9 +49,10 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
                             </p>
                             <button class="item-file-all-metadata-toggle">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#e6e1d6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="8"></line></svg>
-                                Meta
+                                Details
                             </button>
                             <div class="item-file-all-metadata">
+                                <button class="item-file-all-metadata-close">Close</button>
                                 <div id="text-metadata">
                                     <?php echo all_element_texts($file, ['show_element_set_headings' => false]); ?>
                                 </div>
--- src/lib/file-all-meta.js
+++ src/lib/file-all-meta.js
@@ -13,8 +13,38 @@ function fileAllMeta () {
 }
 
 function showMeta (element) {
+  // Close the existing modal
+  if (document.querySelector('.item-file-all-metadata-modal')) {
+    document.querySelector('.item-file-all-metadata-modal').remove()
+  }
+
+  if (document.querySelector('.selected')) {
+    document.querySelector('.selected').classList.remove('selected')
+  }
+
+  // Add a class to the file
+  element.target.parentNode.classList.add('selected')
+
+  // Make a new modal
   let metaContainer = element.target.parentNode.querySelector('.item-file-all-metadata')
-  console.log(metaContainer)
+  let infoModal = document.createElement('div')
+
+  infoModal.classList.add('item-file-all-metadata-modal')
+  infoModal.innerHTML = metaContainer.innerHTML
+
+  infoModal.querySelector('.item-file-all-metadata-close').addEventListener('click', hideMeta)
+
+  // Reveal the modal
+  setTimeout(() => {
+    infoModal.classList.add('open')
+  }, 200)
+
+  document.body.appendChild(infoModal)
+}
+
+function hideMeta (element) {
+  document.querySelector('.selected').classList.remove('selected')
+  element.target.parentNode.remove()
 }
 
 export {fileAllMeta}
