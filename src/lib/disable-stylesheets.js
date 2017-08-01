72e95c1128d8032e9ec0378ced6503e5629ffd35
Christopher Green <cg433n@email.arizona.edu>
2017-08-01 14:10:27 -0700

    Updates exhibit menu, gallery layout styles Refs #24, #13

---
+++ src/lib/disable-stylesheets.js
@@ -0,0 +1,23 @@
+import {forEach} from 'lodash'
+
+// Array, stylesheets to disable
+const disabled = [
+  'plugins/ExhibitBuilder/views/shared/exhibit_layouts/gallery/layout.css'
+]
+
+// Disable stylesheets listed in the `disabled` array
+function disableStylesheets () {
+  forEach(document.styleSheets, (stylesheet) => {
+    // If the stylesheet is external
+    if (stylesheet.href) {
+      disabled.forEach((element) => {
+        // If the stylesheet appears in the `disabled` array, disable it
+        if (stylesheet.href.indexOf(element) >= 0) {
+          stylesheet.disabled = true
+        }
+      })
+    }
+  })
+}
+
+export {disableStylesheets}
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import {loadFonts} from './lib/fonts.js'
 import {exhibitMenu} from './lib/exhibit-menu.js'
 import {itemFilesList} from './lib/item-files-list.js'
 import {fileAllMeta} from './lib/file-all-meta.js'
+import {disableStylesheets} from './lib/disable-stylesheets.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   loadFonts()
   exhibitMenu()
   itemFilesList()
   fileAllMeta()
+  disableStylesheets()
 })
