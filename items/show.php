4c1964a1801e62584d6ff2476c57db23b5aff77d
Christopher Green <cg433n@email.arizona.edu>
2017-08-08 14:33:40 -0700

    Render Soundcloud gallery items Refs #40

--- items/show.php
+++ items/show.php
@@ -6,9 +6,9 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
 <h1 class="item-title"><?php echo metadata('item', 'display_title'); ?></h1>
 <div class="item-details">
 
-    <!-- If this is a 'Book Reader Item" item type, show the UV player -->
     <?php
-    if (metadata('item', 'item_type_name') == "Book Reader Item"){
+    // If this is a 'Book Reader Item" item type, show the UV player
+    if (metadata('item', 'item_type_name') == "Book Reader Item") {
         echo $this->universalViewer($item, array(
             'class' => 'universal-viewer',
             'config' => web_path_to('universal-viewer/config.json'),
@@ -16,10 +16,21 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
     }
     ?>
 
-    <!-- If this is a 'Book Reader Item" item type, don't show the files associated with the item -->
-    <?php if (metadata('item', 'item_type_name') != "Book Reader Item"): ?>
-        <!-- The following returns all of the files associated with an item. -->
-        <?php if (metadata('item', 'has files')): ?>
+    <?php
+    if (metadata('item', 'item_type_name') == "Sound" && function_exists('soundcloud_render')) {
+        // Check the Item to see if it's configured for Soundcloud
+        $relation = metadata('item', array('Dublin Core', 'Relation'));
+        $item_type = metadata('item', 'item_type_name');
+        echo soundcloud_render($relation, $item_type);
+    }
+    ?>
+
+    <?php
+    // If this is a 'Book Reader Item" item type, don't show the files associated with the item
+    if (metadata('item', 'item_type_name') != "Book Reader Item"): ?>
+        <?php
+        // The following returns all of the files associated with an item.
+        if (metadata('item', 'has files')): ?>
             <div id="itemfiles" class="element item-files-list">
                 <h3><?php echo __('Files'); ?></h3>
                 <?php
@@ -48,14 +59,14 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
                                 <?php echo $file_title; ?>
                             </p>
                             <?php if (all_element_texts($file)): ?>
-                            <button class="item-file-all-metadata-toggle">
-                                Details
-                            </button>
-                            <div class="item-file-all-metadata">
-                                <div id="text-metadata">
-                                    <?php echo all_element_texts($file, ['show_element_set_headings' => false]); ?>
+                                <button class="item-file-all-metadata-toggle">
+                                    Details
+                                </button>
+                                <div class="item-file-all-metadata">
+                                    <div id="text-metadata">
+                                        <?php echo all_element_texts($file, ['show_element_set_headings' => false]); ?>
+                                    </div>
                                 </div>
-                            </div>
                             <?php endif; ?>
                         </div>
                     </div>
@@ -67,23 +78,25 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
     <div class="item-meta">
         <?php echo all_element_texts('item', ['show_element_set_headings' => false]); ?>
 
-        <!-- If the item belongs to a collection, the following creates a link to that collection. -->
-        <?php if (metadata('item', 'Collection Name')): ?>
+        <?php
+        // If the item belongs to a collection, the following creates a link to that collection.
+        if (metadata('item', 'Collection Name')): ?>
             <div id="collection" class="element">
                 <h3><?php echo __('Collection'); ?></h3>
                 <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
             </div>
         <?php endif; ?>
 
-        <!-- The following prints a list of all tags associated with the item -->
-        <?php if (metadata('item', 'has tags')): ?>
+        <?php
+        // The following prints a list of all tags associated with the item
+        if (metadata('item', 'has tags')): ?>
             <div id="item-tags" class="element">
                 <h3><?php echo __('Tags'); ?></h3>
                 <div class="element-text"><?php echo tag_string('item'); ?></div>
             </div>
         <?php endif;?>
 
-        <!-- The following prints a citation for this item. -->
+        <?php // The following prints a citation for this item. ?>
         <div id="item-citation" class="element">
             <h3><?php echo __('Citation'); ?></h3>
             <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
