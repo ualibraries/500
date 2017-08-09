7eb9f6ee5dbce456bce6c06362156800706d8158
Christopher Green <cg433n@email.arizona.edu>
2017-08-09 14:03:58 -0700

    Display specific filetypes for items Refs #9765

--- custom.php
+++ custom.php
@@ -1,5 +1,12 @@
 <?php
+/**
+* Custom theme functions
+*/
 
+/**
+* Generates a link for the theme favicon
+* @return String link tag for theme favicon
+*/
 function theme_favicon() {
     $image = get_theme_option('favicon');
     if ($image) {
@@ -11,6 +18,10 @@ function theme_favicon() {
     }
 }
 
+/**
+* Generates an Apple Touch Icon link
+* @return String link tag for theme apple-touch-icon
+*/
 function theme_apple_touch_icon() {
     $image = get_theme_option('apple_touch_icon');
     if ($image) {
@@ -22,6 +33,10 @@ function theme_apple_touch_icon() {
     }
 }
 
+/**
+* Generates a link for the theme ms wide tile
+* @return String link tag for theme ms wide tile
+*/
 function theme_ms_wide_tile() {
     $image = get_theme_option('ms_wide_tile');
     if ($image) {
@@ -33,6 +48,10 @@ function theme_ms_wide_tile() {
     }
 }
 
+/**
+* Generates a link for the theme ms square tile
+* @return String link tag for theme ms square tile
+*/
 function theme_ms_square_tile() {
     $image = get_theme_option('ms_square_tile');
     if ($image) {
@@ -42,4 +61,71 @@ function theme_ms_square_tile() {
         <meta name="msapplication-square558x558logo" content="<?php echo $uri; ?>">
         <?php
     }
+}
+
+/**
+* Generates the item's files
+* @param  Object $item the item
+* @return String list of files
+*/
+function theme_item_files($item, $filetypes = 'images') {
+    if (metadata('item', 'has files')): ?>
+    <div id="itemfiles" class="element item-files-list">
+        <h3><?php echo __('Files'); ?></h3>
+        <?php
+        // Loop through the files
+        set_loop_records('files', $item->Files);
+        foreach (loop('files') as $file) {
+            if ($filetypes == 'image') {
+                theme_item_single_image($file);
+            }
+
+            if ($filetypes == 'pdf' && $file->mime_type == 'application/pdf') {
+                theme_item_single_pdf($file);
+            }
+        }
+        ?>
+    </div>
+    <?php endif;
+}
+
+function theme_item_single_image($file) {
+    ?>
+    <div class="item-file">
+        <div class="item-file-inner">
+            <a href="<?php echo metadata($file, 'uri'); ?>" class="item-file-link">
+                <img src="<?php echo metadata($file, 'fullsize_uri'); ?>" alt="<?php echo metadata($file, array('Dublin Core', 'Title')) ?>" />
+            </a>
+            <p class="item-file-caption">
+                <?php echo metadata($file, array('Dublin Core', 'Title')); ?>
+            </p>
+            <?php if (all_element_texts($file)): ?>
+                <button class="item-file-all-metadata-toggle">
+                    Details
+                </button>
+                <div class="item-file-all-metadata">
+                    <div id="text-metadata">
+                        <?php echo all_element_texts($file, ['show_element_set_headings' => false]); ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+    <?php
+}
+
+function theme_item_single_pdf($file) {
+    ?>
+    <div class="item-file">
+        <div class="item-file-inner">
+            <a href="<?php echo metadata($file, 'uri'); ?>" class="item-file-link">
+                <?php if (metadata($file, array('Dublin Core', 'Title'))): ?>
+                    <?php echo metadata($file, array('Dublin Core', 'Title')); ?>
+                <?php else: ?>
+                    PDF
+                <?php endif; ?>
+            </a>
+        </div>
+    </div>
+    <?php
 }
--- items/show.php
+++ items/show.php
@@ -7,8 +7,8 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
 <div class="item-details">
 
     <?php
-    // If this is a 'Book Reader Item" item type, show the UV player
-    if (metadata('item', 'item_type_name') == "Book Reader Item") {
+    // If this is a 'Book Reader Item' item type, show the UV viewer
+    if (metadata('item', 'item_type_name') == 'Book Reader Item') {
         echo $this->universalViewer($item, array(
             'class' => 'universal-viewer',
             'config' => web_path_to('universal-viewer/config.json'),
@@ -17,6 +17,7 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
     ?>
 
     <?php
+    // If this is a 'Sound Item', display a Soundcloud embed
     if (metadata('item', 'item_type_name') == "Sound" && function_exists('soundcloud_render')) {
         // Check the Item to see if it's configured for Soundcloud
         $relation = metadata('item', array('Dublin Core', 'Relation'));
@@ -26,88 +27,48 @@ echo head(array('title' => $title, 'bodyclass' => 'items show'));
     ?>
 
     <?php
-    // If this is a 'Book Reader Item" item type, don't show the files associated with the item
-    if (metadata('item', 'item_type_name') != "Book Reader Item"): ?>
-        <?php
-        // The following returns all of the files associated with an item.
-        if (metadata('item', 'has files')): ?>
-            <div id="itemfiles" class="element item-files-list">
-                <h3><?php echo __('Files'); ?></h3>
-                <?php
-                set_loop_records('files', $item->Files);
-                foreach (loop('files') as $file):
-                    $file_title = metadata($file, array('Dublin Core', 'Title'));
-                    $file_date = metadata($file, array('Dublin Core', 'Date'));
-                    $file_link_to_original = metadata($file, 'uri');
-                    $file_fullsize_image = metadata($file, 'fullsize_uri');
-                    $file_alt = '';
+    // If this is a 'Book Reader Item' item type, don't show the files associated with the item
+    if (metadata('item', 'item_type_name') != 'Book Reader Item') {
+        theme_item_files($item, 'image');
+    } else {
+        theme_item_files($item, 'pdf');
+    }
+    ?>
 
-                    if ($file_title) {
-                        $file_alt = $file_title;
-
-                        if ($file_date) {
-                            $file_alt .= ', ' . $file_date;
-                        }
-                    }
-                    ?>
-                    <div class="item-file">
-                        <div class="item-file-inner">
-                            <a href="<?php print $file_link_to_original; ?>" class="item-file-link">
-                                <img src="<?php print $file_fullsize_image; ?>" alt="<?php print $file_alt; ?>" />
-                            </a>
-                            <p class="item-file-caption">
-                                <?php echo $file_title; ?>
-                            </p>
-                            <?php if (all_element_texts($file)): ?>
-                                <button class="item-file-all-metadata-toggle">
-                                    Details
-                                </button>
-                                <div class="item-file-all-metadata">
-                                    <div id="text-metadata">
-                                        <?php echo all_element_texts($file, ['show_element_set_headings' => false]); ?>
-                                    </div>
-                                </div>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-            </div>
-        <?php endif; ?>
-    <?php endif; ?>
-
+    <?php // Item meta, (not file meta) ?>
     <div class="item-meta">
-        <?php echo all_element_texts('item', ['show_element_set_headings' => false]); ?>
-
         <?php
+        echo all_element_texts('item', ['show_element_set_headings' => false]);
+
         // If the item belongs to a collection, the following creates a link to that collection.
         if (metadata('item', 'Collection Name')): ?>
-            <div id="collection" class="element">
-                <h3><?php echo __('Collection'); ?></h3>
-                <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
-            </div>
-        <?php endif; ?>
-
-        <?php
-        // The following prints a list of all tags associated with the item
-        if (metadata('item', 'has tags')): ?>
-            <div id="item-tags" class="element">
-                <h3><?php echo __('Tags'); ?></h3>
-                <div class="element-text"><?php echo tag_string('item'); ?></div>
-            </div>
-        <?php endif;?>
-
-        <?php // The following prints a citation for this item. ?>
-        <div id="item-citation" class="element">
-            <h3><?php echo __('Citation'); ?></h3>
-            <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
+        <div id="collection" class="element">
+            <h3><?php echo __('Collection'); ?></h3>
+            <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
         </div>
+    <?php endif; ?>
 
-        <div id="item-output-formats" class="item-output-formats element">
-            <h3><?php echo __('Output Formats'); ?></h3>
-            <div class="element-text"><?php echo output_format_list(); ?></div>
-        </div>
+    <?php
+    // The following prints a list of all tags associated with the item
+    if (metadata('item', 'has tags')): ?>
+    <div id="item-tags" class="element">
+        <h3><?php echo __('Tags'); ?></h3>
+        <div class="element-text"><?php echo tag_string('item'); ?></div>
+    </div>
+    <?php endif;?>
 
-        <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+    <?php // The following prints a citation for this item. ?>
+    <div id="item-citation" class="element">
+        <h3><?php echo __('Citation'); ?></h3>
+        <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
+    </div>
+
+    <div id="item-output-formats" class="item-output-formats element">
+        <h3><?php echo __('Output Formats'); ?></h3>
+        <div class="element-text"><?php echo output_format_list(); ?></div>
+    </div>
+
+    <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
     </div><!--.item-meta -->
 
